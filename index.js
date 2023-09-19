5d1c0ce7a0a2071b7fb9ba193043a43b8a2d9cd2
Tyler Sanders <tsanders5902@gmail.com>
2023-09-19 12:38:05 -0400

    initial

---
+++ index.js
@@ -0,0 +1,50 @@
+// Get references to the button and textarea elements
+const firstButton = document.getElementById('first-button');
+const myTextarea = document.getElementById('myTextarea');
+const secondButton = document.getElementById('second-button');
+const ixiImg = document.getElementById('ixi');
+const ixiText = document.getElementById('ixiText');
+const jackImage = document.getElementById('jack');
+const bananaImage = document.getElementById('banana');
+const jackBananaImage = document.getElementById('jackbanana');
+const caption = document.querySelector("figcaption");
+const thirdButton = document.getElementById('third-button');
+const pukeText = document.getElementById('puke');
+
+// Add a click event listener to the button
+firstButton.addEventListener('click', function() {
+    // Toggle the visibility of the textarea
+    if (myTextarea.style.display === 'none' || myTextarea.style.display === '') {
+        myTextarea.style.display = 'block'; // Show the textarea
+    } else {
+        myTextarea.style.display = 'none'; // Hide the textarea
+    }
+});
+
+secondButton.addEventListener('click', function() {
+    if (ixiImg.style.display === 'none' || ixiImg.style.display === '') {
+        ixiImg.style.display = 'block';
+    } else {
+        ixiImg.style.display = 'none';
+    }
+    if (ixiText.style.display === 'none' || ixiText.style.display === '') {
+        ixiText.style.display = 'block';
+    } else {
+        ixiText.style.display = 'none';
+    }
+})
+
+thirdButton.addEventListener('click', function () {
+    if (pukeText.style.display === 'none' || pukeText.style.display === '') {
+        pukeText.style.display = 'block';
+    } else {
+        pukeText.style.display = 'none';
+    }
+})
+
+bananaImage.addEventListener("click", function () {
+    jackImage.style.display = "none";
+    bananaImage.style.display = "none";
+    caption.style.display = "none";
+    jackBananaImage.style.display = "inline-block";
+})
